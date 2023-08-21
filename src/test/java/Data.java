7c1db0bfc42722bdf00443db3bb3c66e2f13ebb1
Pavel Moshkov <mpo@smart.inreco.ru>
2023-08-21 19:08:17 +0300

    recapcha

--- src/test/java/Data.java
+++ src/test/java/Data.java
@@ -5,9 +5,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Data {
     public String baseUrl = "https://unsplash.com/";
     public String email = "walter_whiteyo@gmail.com";
-            //"moshkov.p2018@gmail.com";
     public String fistname = "walter";
     public String lastname = "white";
     public String password = "my12protected34Pass@@";
+    public String logUrl = "https://unsplash.com/login";
+    public String joinUrl = "https://unsplash.com/join";
 
 }
--- src/test/java/Registration.java
+++ src/test/java/Registration.java
@@ -1,3 +1,5 @@
+import org.asynchttpclient.util.Assertions;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -10,6 +12,7 @@ public class Registration extends Data {
     private By input_fir_name = By.name("user[first_name]");
     private By input_sec_name = By.name("user[last_name]");
     private By email_input = By.name("user[email]");
+    private By username_input = By.name("user[username]");
     private By pass_input = By.name("user[password]");
     private By agree_check = By.className("form-check");
     @Test
@@ -17,14 +20,19 @@ public class Registration extends Data {
 
         WebDriver driver = new ChromeDriver();
         Data data = new Data();
+        driver.manage().window().maximize();
         driver.get(data.baseUrl);
         driver.findElement(signup_button).click();
+        Assert.assertEquals(data.logUrl, driver.getCurrentUrl());
         driver.findElement(create_acc).click();
+        Assert.assertEquals(data.joinUrl, driver.getCurrentUrl());
         driver.findElement(input_fir_name).sendKeys(data.fistname);
         driver.findElement(input_sec_name).sendKeys(data.lastname);
         driver.findElement(email_input).sendKeys(data.email);
         driver.findElement(pass_input).sendKeys(data.password);
+        driver.findElement(username_input).sendKeys("walterwhiteyo");
         driver.findElement(By.name("commit")).click();
-        driver.quit();
+        Assert.assertEquals(data.joinUrl, driver.getCurrentUrl());
+        //driver.quit();
     }
 }
