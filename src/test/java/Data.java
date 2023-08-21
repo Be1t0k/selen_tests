d551749b2a5002fbca77038742845e49b900f08d
Pavel Moshkov <mpo@smart.inreco.ru>
2023-08-21 18:39:56 +0300

    basic auth

--- src/test/java/Data.java
+++ src/test/java/Data.java
@@ -1,3 +1,13 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Data {
+    public String baseUrl = "https://unsplash.com/";
+    public String email = "walter_whiteyo@gmail.com";
+            //"moshkov.p2018@gmail.com";
+    public String fistname = "walter";
+    public String lastname = "white";
+    public String password = "my12protected34Pass@@";
 
 }
--- src/test/java/Registration.java
+++ src/test/java/Registration.java
@@ -1,12 +1,30 @@
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Registration {
+public class Registration extends Data {
 
+    private By signup_button = By.xpath("//*[@id=\"app\"]/div/header/nav/div[2]/div[3]/a");
+    private By create_acc = By.xpath("/html/body/div[2]/div/div/div/div/div[4]/a");
+    private By input_fir_name = By.name("user[first_name]");
+    private By input_sec_name = By.name("user[last_name]");
+    private By email_input = By.name("user[email]");
+    private By pass_input = By.name("user[password]");
+    private By agree_check = By.className("form-check");
+    @Test
     public void registration(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\mpo\\IdeaProjects\\selenium_test\\src\\main\\resources\\chromedriver.exe");
+
         WebDriver driver = new ChromeDriver();
-        driver.get("https://habr.com/ru/articles/");
-        driver.manage().window().maximize();
+        Data data = new Data();
+        driver.get(data.baseUrl);
+        driver.findElement(signup_button).click();
+        driver.findElement(create_acc).click();
+        driver.findElement(input_fir_name).sendKeys(data.fistname);
+        driver.findElement(input_sec_name).sendKeys(data.lastname);
+        driver.findElement(email_input).sendKeys(data.email);
+        driver.findElement(pass_input).sendKeys(data.password);
+        driver.findElement(By.name("commit")).click();
+        driver.quit();
     }
 }
