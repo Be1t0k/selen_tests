4fe73440b8d1cec1b36745c3013fba28c713140a
Pavel Moshkov <mpo@smart.inreco.ru>
2023-08-25 15:31:09 +0300

    final asserts

--- src/test/java/turbosquid/TurboSquid.java
+++ src/test/java/turbosquid/TurboSquid.java
@@ -9,34 +9,42 @@ import pages.LoginPage;
 import pages.MainPage;
 import base.BaseSeleniumTest;
 import pages.SearchPage;
-
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TurboSquid extends BaseSeleniumTest {
     @Test
+    @Order(1)
     public void Login(){
         MainPage mainPage = new MainPage();
         mainPage.MoveToLogin()
                 .InputLoginData(ConfigProvider.EMAIL, ConfigProvider.PASSWORD);
-        Assert.assertTrue(driver.getCurrentUrl().contains(ConfigProvider.LOGIN_URL));
+        Assert.assertTrue(driver.getCurrentUrl().contains(ConfigProvider.URL));
         Assert.assertTrue(driver.findElement(By.className("icon-user")).isDisplayed());
     }
     @Test
+    @Order(2)
     public void IncorrectLogin(){
         LoginPage loginPage = new MainPage().MoveToLogin().WrongInputLoginData(ConfigProvider.INCORRECT_EMAIL, ConfigProvider.PASSWORD);
         Assert.assertTrue(driver.getCurrentUrl().contains(ConfigProvider.LOGIN_URL));
         Assert.assertEquals(loginPage.getErrorMessage(), "invalid email or password.");
     }
+    @Order(4)
     @Test
     public void Search(){
         DetailsPage detailsPage = new MainPage().Search(ConfigProvider.SEARCH_DATA)
                 .OpenDetails()
                 .AddToCart("1");
     }
+    @Order(5)
     @Test
     public void SearchWithChangeType(){
         SearchPage searchPage = new MainPage().ChangeTypeSearch().Search(ConfigProvider.SEARCH_DATA);
         Assert.assertTrue(searchPage.getPriceElement());
         Assert.assertTrue(searchPage.getSearchResult().contains(ConfigProvider.SEARCH_DATA));
     }
+    @Order(3)
     @Test
     public void AddToCartUser(){
         MainPage mainPage = new MainPage();
